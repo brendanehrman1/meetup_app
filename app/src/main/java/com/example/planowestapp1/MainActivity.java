5b95dd0966746c0a88f508e07ae4f76fbd6e8eee
EthanSoccer <ethan.hartfield@gmail.com>
2020-04-28 14:52:25 -0500

    modified 2

--- app/src/main/java/com/example/planowestapp1/MainActivity.java
+++ app/src/main/java/com/example/planowestapp1/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
             b.setY(b.getY() + 2);
         }
     }
-
+    //Ethan was here
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
