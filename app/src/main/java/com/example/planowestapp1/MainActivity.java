f5606732d05b6c3ae3029460739afd5939993572
brendanehrman1 <brendanehrman@yahoo.com>
2020-05-05 14:12:40 -0500

    modified 3

--- app/src/main/java/com/example/planowestapp1/MainActivity.java
+++ app/src/main/java/com/example/planowestapp1/MainActivity.java
@@ -1,32 +1,99 @@
 package com.example.planowestapp1;
 
-import androidx.appcompat.app.AppCompatActivity;
+import java.io.BufferedReader;
+        import java.io.IOException;
+        import java.io.InputStream;
+        import java.io.InputStreamReader;
+        import java.net.HttpURLConnection;
+        import java.net.URL;
 
-import android.os.Bundle;
+        import android.app.Activity;
+        import android.app.ProgressDialog;
+        import android.os.AsyncTask;
+        import android.os.Bundle;
+        import android.view.Menu;
 import android.widget.Button;
-
-import java.util.Timer;
-import java.util.TimerTask;
-
-public class MainActivity extends AppCompatActivity {
-
-    Button b;
-    class Helper extends TimerTask
-    {
-        public void run()
-        {
-            b.setX(b.getX() + 2);
-            b.setY(b.getY() + 2);
-        }
-    }
-
-    @Override
+import android.widget.TextView;
+        import android.view.View;
+public class MainActivity extends Activity {
+    Activity context;
+    TextView tView;
+    ProgressDialog pd;
     protected void onCreate(Bundle savedInstanceState) {
+        //TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Timer timer = new Timer();
-        TimerTask task = new Helper();
-        b = (Button) findViewById(R.id.button);
-        timer.schedule(task, 2000, 10);
+        context=this;
+
+
     }
+
+    public void onStart(){
+        super.onStart();
+
+        BackTask bt=new BackTask();
+        bt.execute("http://www.worldbestlearningcenter.com/example.txt");
+
+
+    }
+
+    //background process to download the file from internet
+    private class BackTask extends AsyncTask<String,Integer,Void>{
+        String text="";
+        protected void onPreExecute(){
+            super.onPreExecute();
+            //display progress dialog
+            pd = new ProgressDialog(context);
+            pd.setTitle("Reading the text file");
+            pd.setMessage("Please wait.");
+            pd.setCancelable(true);
+            pd.setIndeterminate(false);
+            pd.show();
+
+        }
+
+        protected Void doInBackground(String...params){
+            URL url;
+            try {
+                //create url object to point to the file location on internet
+                url = new URL(params[0]);
+                //make a request to server
+                HttpURLConnection con=(HttpURLConnection)url.openConnection();
+                //get InputStream instance
+                InputStream is=con.getInputStream();
+                //create BufferedReader object
+                BufferedReader br=new BufferedReader(new InputStreamReader(is));
+                String line;
+                //read content of the file line by line
+                while((line=br.readLine())!=null){
+                    text+=line;
+
+                }
+
+                br.close();
+
+            }catch (Exception e) {
+                e.printStackTrace();
+                //close dialog if error occurs
+                if(pd!=null) pd.dismiss();
+            }
+
+            return null;
+
+        }
+
+
+        protected void onPostExecute(Void result){
+            //close dialog
+            if(pd!=null)
+                pd.dismiss();
+            tView=(TextView)findViewById(R.id.textView);
+            //display read text in TextView
+            tView.setText(text);
+
+        }
+
+
+    }
+
 }
